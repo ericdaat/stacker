f1db63ca649de35a8b7ed35974878b1b9dac8a3b
Eric Daoud <daoudattoyan.eric@gmail.com>
2018-12-23 11:35:19 +0100

    Add docstring

--- src/core.py
+++ src/core.py
@@ -4,20 +4,55 @@ from sklearn.model_selection import cross_val_score
 
 
 class Stacker(object):
+    """The Stacker class
+    """
     def __init__(self, model_array, stacker_model, n_folds=5):
+        """ Instantiates the Stacker class.
+
+        Args:
+            model_array (list): list of sklearn models
+            stacker_model: meta model that will give the final prediction based on the output
+                of the other models.
+            n_folds (int): number of folds that we divide the dataset into. A model will learn
+                on n-1 folds and predict on the other one.
+        """
         self._model_array = model_array
         self._stacker_model = stacker_model
         self._n_folds = n_folds
 
     def _fill_df_with_fold_ids(self, df):
+        """ Takes a pandas DataFrame and add fold ids to it.
+
+        Args:
+            df (pd.DataFrame): Dataframe that holds the data
+
+        Returns (pd.DataFrame): the dataframe with folds added
+
+        """
         df['fold_id'] = np.random.permutation((df.index % self._n_folds + 1).tolist())
 
         return df
 
     def _make_model_name(self, model):
+        """ Sets a name on a model
+
+        Args:
+            model: sklearn model
+
+        Returns (str): The name of the model
+
+        """
         return 'M_{0}'.format(type(model).__name__)
 
-    def make_meta_df(self, df):
+    def _make_meta_df(self, df):
+        """ Make a meta DataFrame, that is ready for training multiple models.
+
+        Args:
+            df (pd.DataFrame): Input raw dataframe.
+
+        Returns (pd.DataFrame): The input dataframe with folds and NaN predictions.
+
+        """
         meta = df.copy()
         meta = self._fill_df_with_fold_ids(meta)
 
@@ -26,7 +61,18 @@ class Stacker(object):
 
         return meta
 
-    def fill_train_meta_with_predictions(self, meta_df, input_features, labels):
+    def _fill_train_meta_with_predictions(self, meta_df, input_features, labels):
+        """ Make every model output their predictions on the meta dataframe.
+        Note that the final prediction is not yet given.
+
+        Args:
+            meta_df (pd.DataFrame): the input meta dataframe
+            input_features (list): list of input features, corresponding to column names
+                from the meta dataframe.
+            labels (str): name of the column containing the labels to predict
+
+        Returns (pd.DataFrame): DataFrame with predictions from every model.
+        """
         folds_with_predictions = []
 
         for i in range(1, self._n_folds):
@@ -45,7 +91,19 @@ class Stacker(object):
 
         return pd.concat(folds_with_predictions)
 
-    def cross_val_train(self, filled_meta_df, input_features, labels):
+    def _cross_val_train(self, filled_meta_df, input_features, labels):
+        """ Make the final prediction based on the output of every model, using
+            cross validation, returning the mean accuracy.
+
+        Args:
+            filled_meta_df (pd.DataFrame): The meta dataframe, filled with predictions
+                from every model but the final one.
+            input_features (list): List of features, corresponding to column names from
+                the meta dataframe.
+            labels (str): name of the column containing the labels to predict
+
+        Returns (int): Mean accuracy
+        """
         X = np.hstack([filled_meta_df[input_features],
                        pd.concat([pd.get_dummies(filled_meta_df[self._make_model_name(model)])
                                   for model in self._model_array], axis=1)])
@@ -58,10 +116,21 @@ class Stacker(object):
         return np.mean(cv_score)
 
     def fit_predict(self, df, input_features, labels):
-        train_meta = self.make_meta_df(df)
-        train_meta = self.fill_train_meta_with_predictions(train_meta,
-                                                           input_features=input_features,
-                                                           labels=labels)
-        return self.cross_val_train(train_meta,
-                                    input_features=input_features,
-                                    labels=labels)
+        """ Fit and predict, runs the whole pipeline, from data preparation, to the final
+        prediction based on the output of every model to be stacked.
+
+        Args:
+            df (pd.DataFrame): Input dataframe
+            input_features (list): List of features, corresponding to column names from
+                the dataframe.
+            labels (str): name of the column containing the labels to predict
+
+        Returns (int): Mean accuracy on training dataset, using cross validation
+        """
+        train_meta = self._make_meta_df(df)
+        train_meta = self._fill_train_meta_with_predictions(train_meta,
+                                                            input_features=input_features,
+                                                            labels=labels)
+        return self._cross_val_train(train_meta,
+                                     input_features=input_features,
+                                     labels=labels)
